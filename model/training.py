f32c075f3c1b21498d2e7d95903c20aa5f9acb9a
im-victor-mendez <106124660+im-victor-mendez@users.noreply.github.com>
2024-04-14 10:45:21 -0600

    fix: Fixed Neural Network

--- model/training.py
+++ model/training.py
@@ -3,25 +3,26 @@ import random
 import json
 import pickle
 import numpy
+import tensorflow
 
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Activation, Dropout
-from tensorflow.python.keras.optimizer_v1 import SGD
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
 
 nltk.download('punkt')
+nltk.download('wordnet')
 
 print("Training model")
 
 # Minimize [WordNetLemmatizer] use name
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 # Load intents
 intents = json.loads(open("model/intents.json").read())
 
-# Word and letter collections
 words = []
 classes = []
 documents = []
@@ -45,56 +46,49 @@ for intent in intents["intents"]:
 
 # Clean letters to be ignored
 words = [lemmatizer.lemmatize(word)
-         for words in words if word not in ignore_letters]
-
+         for word in words if word not in ignore_letters]
 # Sort a new collection of only one word of each word from [words]
 words = sorted(set(words))
 
 # Sort a new collection of only one class of each class from [classes]
 classes = sorted(set(classes))
 
-pickle.dump(words, open("words.pkl", "wb"))
-pickle.dump(words, open("classes.pkl", "wb"))
+pickle.dump(words, open('model/words.pkl', 'wb'))
+pickle.dump(classes, open('model/classes.pkl', 'wb'))
 
 training = []
-output_empty = [0]*len(classes)
+output_empty = [0] * len(classes)
 
 for document in documents:
     bag = []
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(
         word.lower()) for word in word_patterns]
-
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
 
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
-    training.append([bag, output_row])
+    training.append(bag + output_row)
 
 random.shuffle(training)
 training = numpy.array(training)
 
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+train_x = training[:, :len(words)]
+train_y = training[:, len(words):]
 
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+model.add(Dense(
+    128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation="relu"))
+model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation="softmax"))
+model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss="categorical_crossentropy",
-              optimizer=sgd, metrics=["accuracy"])
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd, metrics=['accuracy'])
 
-model.fit(numpy.array(train_x), numpy.array(
-    train_y), epochs=200, batch_size=5, verbose=1)
-model.save("model/model.model")
-
-print("Documents:")
-print(documents)
-
-print("Train successfully")
-print("Done")
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+model.save('model/model.keras')
+print('Done')
