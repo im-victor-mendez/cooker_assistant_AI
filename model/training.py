d668146b7935cef6fdc599ca786e60fabb18dc9a
im-victor-mendez <106124660+im-victor-mendez@users.noreply.github.com>
2024-04-13 21:57:57 -0600

    ci: Added more training

--- model/training.py
+++ model/training.py
@@ -13,6 +13,8 @@ from tensorflow.python.keras.optimizer_v1 import SGD
 
 nltk.download('punkt')
 
+print("Training model")
+
 # Minimize [WordNetLemmatizer] use name
 lemmatizer = WordNetLemmatizer
 
@@ -25,14 +27,14 @@ classes = []
 documents = []
 ignore_letters = ["?", "¿", ".", ","]
 
-#
+# Train intents
 for intent in intents["intents"]:
     for pattern in intent["patterns"]:
         # Separate each word and create a list of that
         word_list = nltk.word_tokenize(pattern, "Spanish")
 
-        # Insert into [words]
-        words.append(word_list)
+        # Add into [words]
+        words.extend(word_list)
 
         # Insert into documents with [intent["tag"]] name
         documents.append((word_list, intent["tag"]))
@@ -41,7 +43,42 @@ for intent in intents["intents"]:
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
 
-print("Training model")
+# Clean letters to be ignored
+words = [lemmatizer.lemmatize(word)
+         for words in words if word not in ignore_letters]
+
+# Sort a new collection of only one word of each word from [words]
+words = sorted(set(words))
+
+# Sort a new collection of only one class of each class from [classes]
+classes = sorted(set(classes))
+
+pickle.dump(words, open("words.pkl", "wb"))
+pickle.dump(words, open("classes.pkl", "wb"))
+
+training = []
+output_empty = [0]*len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(
+        word.lower()) for word in word_patterns]
+
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = numpy.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
 print("Documents:")
 print(documents)
+
 print("Train successfully")
